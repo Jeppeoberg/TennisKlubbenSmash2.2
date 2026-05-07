4b45fcaa84421239631298d78ff253953730ed1c
Linuskramshoej <linus.kramshoej@protonmail.com>
2026-05-07 11:11:26 +0200

    Updated PaymentStatus

---
+++ src/main/java/model/PaymentStatus.java
@@ -0,0 +1,23 @@
+package model;
+
+public class PaymentStatus {
+    private int memberId;
+    private double annualFee;
+    private boolean paymentComplete;
+    //private LocalDate paymentDate;  Vi gemmer det til senere
+
+    public PaymentStatus(int memberId, double annualFee) {
+        this.paymentComplete = false;
+        this.annualFee = annualFee;
+        this.memberId = memberId;
+    }
+
+    public void setPaymentComplete(boolean b) {
+        this.paymentComplete = b;
+    }
+
+
+    public double getAmountDue() {
+        return this.annualFee;
+    }
+}
