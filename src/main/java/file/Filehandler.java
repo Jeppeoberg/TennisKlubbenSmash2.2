71a6818c0f2f12789ad0f0ec8313279568275095
Jeppeoberg <jeob1000@stud.ek.dk>
2026-05-11 13:17:19 +0200

    Lavede interface i filehandler package med metoder i underklasser

--- src/main/java/file/Filehandler.java
+++ src/main/java/file/Filehandler.java
@@ -1,4 +1,9 @@
 package file;
 
+import java.util.ArrayList;
+
 public interface Filehandler {
+    void saveToFile();
+    ArrayList readFile();
+
 }
--- src/main/java/file/TrainingResultsFH.java
+++ src/main/java/file/TrainingResultsFH.java
@@ -1,4 +1,15 @@
 package file;
 
+import java.util.ArrayList;
+
 public class TrainingResultsFH implements Filehandler {
+    @Override
+    public ArrayList readFile() {
+        return null;
+    }
+    @Override
+    public void saveToFile() {
+
+    }
 }
+
